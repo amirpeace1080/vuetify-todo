fec6222ff131a8827cce64a25080f7d77fb75233
amirpeace1080 <amirpeace1080@gmail.com>
2021-10-28 15:06:38 +0330

    add  - item

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="home">
+    <v-text-field
+      v-model="newTaskTitle"
+      @click:append="addTask"
+      @keyup.enter="addTask"
+      class="pa-3"
+      outlined
+      label="Add Task"
+      hide-details
+      clearable
+      append-icon="mdi-plus"></v-text-field>
+
+
     <v-list class="pt-0" flat>
       <div v-for="task in tasks" :key="task.id">
         <v-list-item @click="doneTask(task.id)"
@@ -42,14 +54,24 @@
 export default {
   data() {
     return {
+      newTaskTitle: '',
       tasks: [
-        { id: 1, title: "Wake up", done: false },
-        { id: 2, title: "Get bananas", done: false },
-        { id: 3, title: "Eat bananas", done: false },
+        // { id: 1, title: "Wake up", done: false },
+        // { id: 2, title: "Get bananas", done: false },
+        // { id: 3, title: "Eat bananas", done: false },
       ],
     };
   },
   methods:{
+    addTask(){
+      let newTask ={
+        id: Date.now(),
+        title: this.newTaskTitle,
+        done: false
+      }
+      this.tasks.push(newTask)
+      this.newTaskTitle = ''
+    },
     doneTask(id){
       let task = this.tasks.filter(task => task.id === id)[0]
       task.done =! task.done
