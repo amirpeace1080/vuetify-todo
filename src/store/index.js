632b75461600fa5244078bb6c545dee221709290
amirpeace1080 <amirpeace1080@gmail.com>
2021-10-31 21:02:17 +0330

    show snackbar when task deleted

--- src/store/index.js
+++ src/store/index.js
@@ -41,6 +41,11 @@ export default new Vuex.Store({
     addTask({commit, newTaskTitle}){
       commit('addTask', newTaskTitle)
       commit('showSnackbar')
+    },
+    
+    deleteTask({commit}, id){
+      commit('deleteTask', id)
+      commit('showSnackbar')
     }
   },
   getters: {},
