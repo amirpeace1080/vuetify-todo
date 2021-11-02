07b451c42dd25a948106d6624adaa838339a85d1
amirpeace1080 <amirpeace1080@gmail.com>
2021-11-02 16:09:19 +0330

    improve the style

--- src/App.vue
+++ src/App.vue
@@ -58,17 +58,9 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
 
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
+      <Search />
 
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
@@ -81,10 +73,12 @@
 
 <script>
 import Snackbar from './components/Shared/Snackbar.vue'
+import Search from './components/Tools/Search.vue'
 
 export default {
   components: {
-    Snackbar
+    Snackbar,
+    Search
   },
   data(){
     return{
---
+++ src/components/Tools/Search.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-text-field
+    class="expanding-search mt-1"
+    placeholder="Search"
+    prepend-inner-icon="mdi-magnify"
+    filled
+    dense
+    clearable
+    ></v-text-field>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="sass">
+    .expanding-search
+        .v-input__slot
+            &:befor &:after
+                border-color: transparent
+</style>
